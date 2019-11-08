7bfd1f1fbc977b6e8f93fcadc495b4fc631b9a25
Vivek Reddy Karri <vkarri@ncsu.edu>
2019-11-08 00:21:41 -0500

    Tests folder created

---
+++ tests/sampleTest.java
@@ -0,0 +1,30 @@
+import utilities.*;
+import java.sql.*;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import java.io.*;
+
+public class Test {
+	public static void main(String[] args) throws SQLException, FileNotFoundException {
+		SQLExec CDB = new SQLExec();
+		CDB.connect("vkarri", "200315262");
+		
+		FileReader path = new FileReader("/afs/unity.ncsu.edu/users/v/vkarri/HMS/sql/samplescript.sql");
+		
+		CDB.execCommandScript(path);
+
+		String cmd = "SELECT COF_NAME, PRICE FROM COFFEES1";
+		ResultSet rs = CDB.execQuery(cmd);
+
+		while (rs.next()) {
+		    String s = rs.getString("COF_NAME");
+		    float n = rs.getFloat("PRICE");
+		    System.out.println(s + "   " + n);
+		}
+
+		rs.close();
+
+		CDB.terminate();
+	}
+}
+
+
