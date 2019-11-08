c35ce2ece70c3656018cf1622f1fc37ea7c6bcee
Vivek Reddy Karri <vkarri@ncsu.edu>
2019-11-07 23:56:34 -0500

    fixed untracked files

--- setup.sh
+++
@@ -1,4 +0,0 @@
-export CLASSPATH=".:/afs/bp.ncsu.edu/dist/oracle10g/.install/i386_linux26/OraHome1/jdbc/lib classes12.zip":":/afs/unity.ncsu.edu/users/v/vkarri/HMS":"${CLASSPATH}"
-export CLASSPATH="/afs/unity.ncsu.edu/users/v/vkarri/HMS/lib/*":"${CLASSPATH}"
-alias java="java -cp .:/afs/bp.ncsu.edu/dist/oracle10g/.install/i386_linux26/OraHome1/jdbc/lib/classes12.zip"
-add jdk
