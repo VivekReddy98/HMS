bf6cc4ee0040252ef1b0d00a57e00e57e97c4f14
Vivek Reddy Karri <vkarri@ncsu.edu>
2019-11-15 02:51:12 -0500

    Custom Data Structure for Report Made

---
+++ utilities/ReportDS.java
@@ -0,0 +1,24 @@
+package utilities;
+
+import java.sql.*;
+import java.text.MessageFormat;
+import java.util.*;
+import java.io.*;
+import utilities.*;
+
+public class ReportDS {
+	public String discharge_status;
+	public String trmt_description;
+	public int f_id = 0;
+	public String e_id;
+	public ArrayList<ArrayList< String>> Referral_Reasons;
+	public int n_code;
+	public String n_description;
+
+	public ArrayList<String> Q_Ref_Reasons;
+	public String Q_Ref_to;
+	public String Q_trmt;
+	public String Q_discharge;
+	public String Q_negex;
+
+}
